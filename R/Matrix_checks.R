93d1614aba600c451b278f129c14384bdbf5ee28
alexanderhayward1995 <111029333+alexanderhayward1995@users.noreply.github.com>
2023-08-15 11:29:04 +1200

    Update Matrix_checks.R

--- R/Matrix_checks.R
+++ R/Matrix_checks.R
@@ -12,8 +12,11 @@
 
 
 Matrix_checks <- function(S, F){
-Si <- sapply(S, is.character)
-S <- S[, !Si]
+Si <- which(sapply(S, is.character))
+if (length(Si) > 0) {
+  # Remove columns with character values
+  S <- S[, -Si]
+}
   F <- subset(F, select = c(colnames(S)))
   ba <- rownames(F)
   ba1<- which(ba =="Syn")
